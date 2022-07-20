4e9a0f0af130687747956c267aab05b6fec1aeeb
Sean <dr.creator@gmail.com>
2022-07-21 01:42:59 +0500

    initial code

---
+++ javascript/nextjs/src/components/svg-icon.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import SVG from 'react-inlinesvg';
+import PropTypes from 'prop-types';
+
+export const SvgIcon = (icon, path = 'media/icons/duotune/') => {
+  return (
+    <SVG src={path + icon}  height={'1em'} width={'auto'}/>
+  )
+};
