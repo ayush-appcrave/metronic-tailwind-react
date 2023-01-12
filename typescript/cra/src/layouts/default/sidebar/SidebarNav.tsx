1b1a0c5391b5357efeb4f7725d4758235bb7815a
Sean <dr.creator@gmail.com>
2023-01-12 10:05:15 +0500

    latest code

--- typescript/cra/src/layouts/default/sidebar/SidebarNav.tsx
+++ typescript/cra/src/layouts/default/sidebar/SidebarNav.tsx
@@ -1,18 +1,25 @@
 import { Box, List, ListSubheader } from '@mui/material';
 import { useDefaultLayout } from '..';
+import { useViewport } from '../../../hooks';
 import Scrollbar from '../../../components/scrollbar';
 import { Nav, NavItem, NavConfigType, NavVerticalStylesConfig } from '../../../components/nav';
 import { NAV_VERTICAL } from "../../../config/navs.config";
+import { number } from 'yup';
 
 const SidebarNav = () => {
   const {isSidebarExpand, isSidebarCollapse} = useDefaultLayout();
 	const styles = NavVerticalStylesConfig();
+	const [width, height]= useViewport();
+
+	console.log('height:' + height);
+
+	const scrollableHeight: number = height - 100;
 
   return (
 		<Scrollbar 
 			sx={{
-				height: 800,
-				overflow: 'hidden',
+				height: scrollableHeight,
+				//overflow: 'hidden',
 				px: 1.5,
 				mx: 1
 			}}   
