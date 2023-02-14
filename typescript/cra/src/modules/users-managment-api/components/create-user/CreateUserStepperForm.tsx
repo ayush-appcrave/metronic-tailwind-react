d4f1b6754043c912e030a7f7c6c2063a5e5548e9
laurisstepanovs <33573735+laurisstepanovs@users.noreply.github.com>
2023-02-15 00:39:35 +0700

    feat: added stepper validation

--- typescript/cra/src/modules/users-managment-api/components/create-user/CreateUserStepperForm.tsx
+++ typescript/cra/src/modules/users-managment-api/components/create-user/CreateUserStepperForm.tsx
@@ -11,8 +11,12 @@ import {User} from "../../core/_models";
 import {createUser} from "../../core/_requests";
 import {Close} from "@mui/icons-material";
 import {useQueryResponse} from "../../core/QueryResponseProvider";
+import {createAccountSchemas, inits} from './CreateUserStepperFormHelper'
+import {Form, Formik, FormikProps, FormikValues} from "formik";
+import {useSnackbar} from "notistack";
+import axios from "axios";
 
-const steps = ['General', 'Auth info'];
+const steps = ['General', 'Auth info', 'Review'];
 
 interface CreateUserFormProps {
     open: boolean;
@@ -20,29 +24,12 @@ interface CreateUserFormProps {
 }
 
 export default function CreateUserStepperForm(props: CreateUserFormProps) {
-    const {refetch} = useQueryResponse();
+    const { refetch } = useQueryResponse();
+    const { enqueueSnackbar } = useSnackbar();
     const [activeStep, setActiveStep] = React.useState(0);
+    const [currentSchema, setCurrentSchema] = useState(createAccountSchemas[activeStep])
+    const [initValues] = useState<User>(inits)
     const [skipped, setSkipped] = React.useState(new Set<number>());
-
-    const formInitData = {
-        id: "",
-        first_name: "",
-        last_name: "",
-        email: "",
-        role: "user",
-        two_steps_auth: false,
-        password: "",
-        password_confirmation: "",
-    }
-
-    const [formData, setFormData] = useState<User>(formInitData);
-
-    const handleChange = (name:string, value:unknown) => {
-        setFormData((prevState)=>({
-            ...prevState,
-            [name]: value,
-        } as User));
-    }
 
     const isStepSkipped = (step: number) => {
         return skipped.has(step);
@@ -54,29 +41,30 @@ export default function CreateUserStepperForm(props: CreateUserFormProps) {
             newSkipped = new Set(newSkipped.values());
             newSkipped.delete(activeStep);
         }
-
         setActiveStep((prevActiveStep) => prevActiveStep + 1);
+        setCurrentSchema(createAccountSchemas[activeStep + 1])
         setSkipped(newSkipped);
     };
 
     const handleBack = () => {
         setActiveStep((prevActiveStep) => prevActiveStep - 1);
+        setCurrentSchema(createAccountSchemas[activeStep])
     };
 
-    const handleReset = () => {
-        setFormData(formInitData);
-        setActiveStep(0);
-    };
 
-    const submit = async () => {
+    const submit = async (values: User) => {
         try {
-            await createUser(formData);
+            await createUser(values);
             refetch();
             props.handleClose();
-            alert("User was successfully created.");
+            enqueueSnackbar("User was successfully created.", { variant: "success" })
             handleNext();
-        } catch (err) {
-            console.log(err);
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                enqueueSnackbar(JSON.stringify(Object.values(error.response?.data.message)[0]?.toString()), { variant: "error" });
+            } else {
+                enqueueSnackbar("Something went wrong!", { variant: "error" });
+            }
         }
     }
 
@@ -106,47 +94,45 @@ export default function CreateUserStepperForm(props: CreateUserFormProps) {
                         );
                     })}
                 </Stepper>
-                {activeStep === steps.length ? (<React.Fragment>
-                    <Typography sx={{ mt: 2, mb: 1 }}>
-                        All steps completed - you&apos;re finished
-                    </Typography>
-                    <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-                        <Box sx={{ flex: '1 1 auto' }} />
-                        <Button onClick={handleReset}>Reset</Button>
-                    </Box>
-                </React.Fragment>) : <React.Fragment>
+                <React.Fragment>
                     <Box sx={{
                         margin: "20px"
                     }}>
-                        {renderSteps(activeStep, handleChange, formData)}
-                    </Box>
-                    <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-                        <Button
-                            color="inherit"
-                            disabled={activeStep === steps.length || activeStep === 0}
-                            onClick={handleBack}
-                            sx={{ mr: 1 }}
-                        >
-                            Back
-                        </Button>
-                        <Box sx={{ flex: '1 1 auto' }} />
+                        <Formik validationSchema={currentSchema} initialValues={initValues} onSubmit={handleNext}>
+                            {(props)=>(
+                                    <Form onSubmit={props.handleSubmit}>
+                                        {renderSteps(activeStep, props)}
+                                        <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                                            <Button
+                                                color="inherit"
+                                                disabled={activeStep === steps.length || activeStep === 0}
+                                                onClick={handleBack}
+                                                sx={{ mr: 1 }}
+                                            >
+                                                Back
+                                            </Button>
+                                            <Box sx={{ flex: '1 1 auto' }} />
 
-                        { activeStep === steps.length - 1 && <Button onClick={submit}>
-                            Submit
-                        </Button> }
+                                            { activeStep === steps.length - 1 && <Button onClick={(e)=>submit(props.values)}>
+                                                Submit
+                                            </Button> }
 
-                        { activeStep !== steps.length - 1 && <Button onClick={handleNext}>
-                            Continue
-                        </Button> }
+                                            { activeStep !== steps.length - 1 && <Button type="submit">
+                                                Continue
+                                            </Button> }
+                                        </Box>
+                                    </Form>
+                            )
+                            }
+                        </Formik>
                     </Box>
                 </React.Fragment>
-                }
             </>
         </Box>
     );
 }
 
-function renderSteps(step: number, handleChange:Function, formData:User){
+function renderSteps(step: number, props:FormikProps<User>){
     switch(step) {
         case 0:
             return <>
@@ -156,9 +142,12 @@ function renderSteps(step: number, handleChange:Function, formData:User){
                         label="First Name"
                         id="first_name"
                         name="first_name"
-                        value={formData?.first_name}
-                        onChange={(e)=>handleChange(e.target.name, e.target.value)}
                         variant="outlined"
+                        error={!!props.errors.first_name && props.touched.first_name}
+                        onChange={props.handleChange}
+                        onBlur={props.handleBlur}
+                        value={props.values.first_name}
+                        helperText={props.touched.first_name && props.errors.first_name ? props.errors.first_name : ""}
                     />
                 </FormGroup>
                 <FormGroup sx={{ marginY: "5px", width: "100%",  }}>
@@ -167,9 +156,12 @@ function renderSteps(step: number, handleChange:Function, formData:User){
                         label="Last Name"
                         id="last_name"
                         name="last_name"
-                        value={formData?.last_name}
-                        onChange={(e)=>handleChange(e.target.name, e.target.value)}
                         variant="outlined"
+                        error={!!props.errors.last_name && props.touched.last_name}
+                        onChange={props.handleChange}
+                        onBlur={props.handleBlur}
+                        value={props.values.last_name}
+                        helperText={props.touched.last_name && props.errors.last_name ? props.errors.last_name : ""}
                     />
                 </FormGroup>
                 <FormGroup sx={{ marginY: "5px", width: "100%",  }}>
@@ -178,20 +170,24 @@ function renderSteps(step: number, handleChange:Function, formData:User){
                         label="Email"
                         id="email"
                         name="email"
-                        value={formData?.email}
                         variant="outlined"
-                        onChange={(e)=>handleChange(e.target.name, e.target.value)}
+                        error={!!props.errors.email && props.touched.email}
+                        onChange={props.handleChange}
+                        onBlur={props.handleBlur}
+                        value={props.values.email}
+                        helperText={props.touched.email && props.errors.email ? props.errors.email : ""}
                     />
                 </FormGroup>
-                <FormControl sx={{ marginY: "5px", width: "100%",  }}>
+                <FormControl error={!!props.errors.role && props.touched.role} sx={{ marginY: "5px", width: "100%", }}>
                     <InputLabel id="kt-role-select-label">Age</InputLabel>
                     <Select
                         labelId="kt-role-select-label"
                         id="kt-role-select"
-                        value={formData?.role}
                         name="role"
                         label="user"
-                        onChange={(e)=>handleChange(e.target.name, e.target.value)}
+                        onChange={props.handleChange}
+                        onBlur={props.handleBlur}
+                        value={props.values.role}
                     >
                         <MenuItem value="user">User</MenuItem>
                         <MenuItem value="admin">Admin</MenuItem>
@@ -206,9 +202,12 @@ function renderSteps(step: number, handleChange:Function, formData:User){
                         label="Password"
                         id="password"
                         name="password"
-                        value={formData?.password}
                         variant="outlined"
-                        onChange={(e)=>handleChange(e.target.name, e.target.value)}
+                        error={!!props.errors.password && props.touched.password}
+                        onChange={props.handleChange}
+                        onBlur={props.handleBlur}
+                        value={props.values.password}
+                        helperText={props.touched.password && props.errors.password ? props.errors.password : ""}
                     />
                 </FormGroup>
                 <FormGroup sx={{ marginY: "5px", width: "100%",  }}>
@@ -217,14 +216,25 @@ function renderSteps(step: number, handleChange:Function, formData:User){
                         label="Password Confirmation"
                         id="password_confirmation"
                         name="password_confirmation"
-                        value={formData?.password_confirmation}
                         variant="outlined"
-                        onChange={(e)=>handleChange(e.target.name, e.target.value)}
+                        error={!!props.errors.password_confirmation && props.touched.password_confirmation}
+                        onChange={props.handleChange}
+                        onBlur={props.handleBlur}
+                        value={props.values.password_confirmation}
+                        helperText={props.touched.password_confirmation && props.errors.password_confirmation ? props.errors.password_confirmation : ""}
                     />
                 </FormGroup>
                 <FormGroup sx={{ marginY: "5px", width: "100%",  }}>
-                    <FormControlLabel control={<Switch name="two_steps_auth" id="two_steps_auth" checked={formData?.two_steps_auth} value={formData?.two_steps_auth} onChange={(e)=>handleChange(e.target.name, e.target.value !== 'true')} />} label="Two Steps Auth" />
+                    <FormControlLabel control={<Switch name="two_steps_auth" id="two_steps_auth" checked={props.values.two_steps_auth} value={props.values.two_steps_auth} onChange={props.handleChange} />} label="Two Steps Auth" />
                 </FormGroup>
+            </>
+        case 2:
+            return <>
+                Review data before submitting
+                <Typography>First name: {props.values.first_name}</Typography>
+                <Typography>Last name: {props.values.last_name}</Typography>
+                <Typography>Email: {props.values.email}</Typography>
+                <Typography>Role: {props.values.role}</Typography>
             </>
     }
 }
