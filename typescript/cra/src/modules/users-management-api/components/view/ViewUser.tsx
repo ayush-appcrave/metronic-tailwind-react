2d4b6a14c3bbd0260db760572069aa451a5e71b7
laurisstepanovs <33573735+laurisstepanovs@users.noreply.github.com>
2023-04-17 12:24:18 +0800

    feat: added skeleton for user view component

--- typescript/cra/src/modules/users-management-api/components/view/ViewUser.tsx
+++ typescript/cra/src/modules/users-management-api/components/view/ViewUser.tsx
@@ -6,7 +6,7 @@ import {useEffect, useState} from "react";
 import {User} from "../../core/_models";
 import {deleteUser, getUserById} from "../../core/_requests";
 import {QUERIES} from "../../helpers";
-import {Avatar, Button, Typography} from "@mui/material";
+import {Avatar, Box, Button, Typography, Skeleton} from "@mui/material";
 import {toAbsoluteUrl} from "utils";
 
 type Props = {
@@ -18,6 +18,7 @@ function ViewUser(props: Props) {
     const {query} = useQueryResponse()
     const queryClient = useQueryClient()
     const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
     const [currentUser, setCurrentUser] = useState<User>({
         id: "",
         first_name: "",
@@ -43,24 +44,41 @@ function ViewUser(props: Props) {
 
     useEffect( ()=>{
         if(props.userId){
+            setLoading(true);
             getUserById(props.userId).then(user=>{
                 setCurrentUser(user as User);
-            })
+                setLoading(false);
+            });
         }
     }, []);
     return <>
-        <Typography>User</Typography>
+        { !loading ? <Box>
+            <Typography>User</Typography>
 
-        <Typography>{currentUser.id}</Typography>
-        <Avatar alt={currentUser.first_name} src={toAbsoluteUrl('/media/avatars/300-1.jpg')} />
-        <Typography>{currentUser.first_name} {currentUser.last_name}</Typography>
-        <Typography>{currentUser.role}</Typography>
-        <Typography>{currentUser.created_at}</Typography>
-        <Typography>{currentUser.email}</Typography>
+            <Typography>{currentUser.id}</Typography>
+            <Avatar alt={currentUser.first_name} src={toAbsoluteUrl('/media/avatars/300-1.jpg')} />
+            <Typography>{currentUser.first_name} {currentUser.last_name}</Typography>
+            <Typography>{currentUser.role}</Typography>
+            <Typography>{currentUser.created_at}</Typography>
+            <Typography>{currentUser.email}</Typography>
 
-        <Button variant="outlined" color="info" onClick={navigateUserEditPage}>Edit User</Button>
-        <Button variant="outlined" color="error" onClick={async () => await deleteItem.mutateAsync()}>Delete</Button>
-    </>;
+            <Button variant="outlined" color="info" onClick={navigateUserEditPage}>Edit User</Button>
+            <Button variant="outlined" color="error" onClick={async () => await deleteItem.mutateAsync()}>Delete</Button>
+        </Box> : <Box>
+            <Skeleton variant="text" width={"30%"} sx={{ fontSize: '1rem' }} />
+
+            <Skeleton variant="text" width={"5%"} sx={{ fontSize: '1rem' }} />
+            <Skeleton variant="circular" width={40} height={40} />
+            <Skeleton variant="text" width={"50%"} sx={{ fontSize: '1rem' }} />
+            <Skeleton variant="text" width={"15%"} sx={{ fontSize: '1rem' }} />
+            <Skeleton variant="text" width={"60%"} sx={{ fontSize: '1rem' }} />
+            <Skeleton variant="text" width={"55%"} sx={{ fontSize: '1rem' }} />
+
+            <Button variant="outlined" color="info" onClick={navigateUserEditPage}>Edit User</Button>
+            <Button variant="outlined" color="error" onClick={async () => await deleteItem.mutateAsync()}>Delete</Button>
+        </Box>
+        }
+   </>;
 }
 
 export { ViewUser }
