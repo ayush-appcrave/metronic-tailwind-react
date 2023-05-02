1f7f08dfefaf61a2c6196cb4b9bb5207f87da0aa
laurisstepanovs <33573735+laurisstepanovs@users.noreply.github.com>
2023-05-02 18:54:04 +0800

    feat: updated a default CRUD loader

--- typescript/cra/src/modules/users-management-api/components/UserManagementTableContainer.tsx
+++ typescript/cra/src/modules/users-management-api/components/UserManagementTableContainer.tsx
@@ -24,10 +24,11 @@ import {
   useQueryResponsePagination
 } from '../core/QueryResponseProvider';
 import { useQueryRequest } from '../core/QueryRequestProvider';
-import { TableLoader } from './loading/TableLoader';
 import { useSearchParams } from 'react-router-dom';
 import qs from 'query-string';
 import { initialQueryRequest } from '../helpers';
+import { TableSkeleton } from './loading/TableSkeleton';
+import { TableLoader } from './loading/TableLoader';
 
 interface Props {
   denseKey: string;
@@ -119,77 +120,82 @@ const UserManagementTableContainer = (props: Props) => {
             sx={{
               position: 'relative'
             }}>
-            {isLoading ? (
-              <TableLoader
-                itemsPerPage={pagination.items_per_page ? pagination.items_per_page : 10}
-                rowHeight={dense ? 49 : 69}></TableLoader>
-            ) : (
-              data.map((row, index) => {
-                const labelId = `enhanced-table-checkbox-${index}`;
+            {data.map((row, index) => {
+              const labelId = `enhanced-table-checkbox-${index}`;
 
-                return (
-                  <TableRow
-                    hover
-                    role="checkbox"
-                    aria-checked={isSelected(row.id)}
-                    tabIndex={-1}
-                    key={row.id}
-                    selected={isSelected(row.id)}>
-                    <TableCell padding="checkbox">
-                      <Checkbox
-                        color="primary"
-                        size="small"
-                        onInput={(event) => {
-                          onSelect(row.id);
-                        }}
-                        checked={isSelected(row.id)}
-                        inputProps={{
-                          'aria-labelledby': labelId
-                        }}
-                      />
-                    </TableCell>
-                    <TableCell width={'20%'} component="th" id={labelId} scope="row" padding="none">
+              return (
+                <TableRow
+                  hover
+                  role="checkbox"
+                  aria-checked={isSelected(row.id)}
+                  tabIndex={-1}
+                  key={row.id}
+                  selected={isSelected(row.id)}>
+                  <TableCell padding="checkbox">
+                    <Checkbox
+                      color="primary"
+                      size="small"
+                      onInput={(event) => {
+                        onSelect(row.id);
+                      }}
+                      checked={isSelected(row.id)}
+                      inputProps={{
+                        'aria-labelledby': labelId
+                      }}
+                    />
+                  </TableCell>
+                  <TableCell width={'20%'} component="th" id={labelId} scope="row" padding="none">
+                    <Box
+                      sx={{
+                        display: 'flex'
+                      }}>
+                      {row.avatar && (
+                        <Avatar alt={row.first_name} src={toAbsoluteUrl(row.avatar)} />
+                      )}
+
                       <Box
                         sx={{
-                          display: 'flex'
+                          verticalAlign: 'middle',
+                          marginTop: 'auto',
+                          marginBottom: 'auto',
+                          marginLeft: '5px'
                         }}>
-                        {row.avatar && (
-                          <Avatar alt={row.first_name} src={toAbsoluteUrl(row.avatar)} />
-                        )}
-
-                        <Box
-                          sx={{
-                            verticalAlign: 'middle',
-                            marginTop: 'auto',
-                            marginBottom: 'auto',
-                            marginLeft: '5px'
-                          }}>
-                          {row.first_name}
-                        </Box>
+                        {row.first_name}
                       </Box>
-                    </TableCell>
-                    <TableCell width={'15%'} align="left">
-                      {row.last_name}
-                    </TableCell>
-                    <TableCell width={'10%'} align="left">
-                      {row.role}
-                    </TableCell>
-                    <TableCell width={'10%'} align="left">
-                      {row.status}
-                    </TableCell>
-                    <TableCell width={'10%'} align="left">
-                      {row.two_steps_auth ? 'enabled' : 'disabled'}
-                    </TableCell>
-                    <TableCell width={'20%'} align="left">
-                      {formatDate(row.created_at)}
-                    </TableCell>
-                    <TableCell width={'10%'} align="left">
-                      {props.children(row.id)}
-                    </TableCell>
-                  </TableRow>
-                );
-              })
-            )}
+                    </Box>
+                  </TableCell>
+                  <TableCell width={'15%'} align="left">
+                    {row.last_name}
+                  </TableCell>
+                  <TableCell width={'10%'} align="left">
+                    {row.role}
+                  </TableCell>
+                  <TableCell width={'10%'} align="left">
+                    {row.status}
+                  </TableCell>
+                  <TableCell width={'10%'} align="left">
+                    {row.two_steps_auth ? 'enabled' : 'disabled'}
+                  </TableCell>
+                  <TableCell width={'20%'} align="left">
+                    {formatDate(row.created_at)}
+                  </TableCell>
+                  <TableCell width={'10%'} align="left">
+                    {props.children(row.id)}
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+            {isLoading &&
+              (data.length ? (
+                <TableLoader
+                  itemsPerPage={pagination.items_per_page ? pagination.items_per_page : 10}
+                  rowHeight={dense ? 49 : 69}></TableLoader>
+              ) : (
+                <TableSkeleton
+                  itemsPerPage={
+                    pagination.items_per_page ? pagination.items_per_page : 10
+                  }></TableSkeleton>
+              ))}
             {!pagination.total && !isLoading && (
               <TableRow>
                 <TableCell colSpan={headCells.length}>No data found</TableCell>
--- typescript/cra/src/modules/users-management-api/components/loading/TableLoader.tsx
+++ typescript/cra/src/modules/users-management-api/components/loading/TableLoader.tsx
@@ -10,15 +10,26 @@ const TableLoader = (props: Props) => {
     <TableRow
       sx={{
         height: `${props.rowHeight * props.itemsPerPage}px`,
-        position: 'relative'
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0
       }}>
       <TableCell
         colSpan={8}
         rowSpan={props.itemsPerPage}
         sx={{
           textAlign: 'center',
-          display: 'table-cell',
-          verticalAlign: 'middle'
+          verticalAlign: 'middle',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'black',
+          opacity: 0.3,
+          display: 'flex',
+          width: '100%',
+          height: '100%',
+          cursor: 'wait'
         }}>
         <LinearProgress sx={{ marginLeft: 'auto', marginRight: 'auto', width: '30%' }} />
       </TableCell>
