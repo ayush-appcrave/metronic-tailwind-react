bfb1b2d39c34984a5ffc4d6989324da98e3589a7
laurisstepanovs <33573735+laurisstepanovs@users.noreply.github.com>
2023-09-04 08:35:08 +0300

    feat: fixes for auth module

--- typescript/cra/src/auth/AuthPage.tsx
+++ typescript/cra/src/auth/AuthPage.tsx
@@ -4,29 +4,27 @@ import { AuthLayout } from '../layouts/auth';
 // Auth0 login page
 // import { Auth0Login } from './components/auth0';
 // Firebase auth pages
-import { FirebaseLogin, FirebaseRegistration } from './components/firebase';
+// import { FirebaseLogin, FirebaseRegistration } from './components/firebase';
 // JWT auth pages
-// import { ForgotPassword, Login, Registration } from './components/jwt';
+import { ForgotPassword, Login, Registration } from './components/jwt';
 
 const AuthPage = () => (
   <Routes>
     <Route element={<AuthLayout />}>
-      {/* <Route path="login" element={<Login />} />
+      <Route path="login" element={<Login />} />
       <Route path="registration" element={<Registration />} />
       <Route path="forgot-password" element={<ForgotPassword />} />
-      <Route index element={<Login />} /> */}
+      <Route index element={<Login />} />
 
       {/* <Route path="login" element={<Auth0Login />} />
       <Route index element={<Auth0Login />} /> */}
 
-      <Route path="login" element={<FirebaseLogin />} />
+      {/* <Route path="login" element={<FirebaseLogin />} />
       <Route path="registration" element={<FirebaseRegistration />} />
-      <Route index element={<FirebaseLogin />} />
+      <Route index element={<FirebaseLogin />} /> */}
 
       <Route path="*" element={<Navigate to="/error/404" />} />
     </Route>
-
-
   </Routes>
 );
 
--- typescript/cra/src/auth/useAuthContext.ts
+++ typescript/cra/src/auth/useAuthContext.ts
@@ -1,6 +1,8 @@
 import { useContext } from 'react';
 
-import { AuthContext } from './providers/FirebaseProvider';
+import { AuthContext } from './providers/JWTProvider';
+// import { AuthContext } from './providers/Auth0Provider';
+// import { AuthContext } from './providers/FirebaseProvider';
 
 export const useAuthContext = () => {
   const context = useContext(AuthContext);
