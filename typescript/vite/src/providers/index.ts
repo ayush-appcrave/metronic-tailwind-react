c4b6d3f0b0d8c489310feaf2a20e79600b1fcf39
Sean <dr.creator@gmail.com>
2024-01-20 01:15:34 +0500

    tailwind integration fix

--- typescript/vite/src/providers/index.ts
+++ typescript/vite/src/providers/index.ts
@@ -2,6 +2,4 @@ export * from './LayoutsProvider';
 export * from './LoadersProvider';
 export * from './SettingsProvider';
 export * from './SnackbarProvider';
-export * from './ThemeProvider';
-export * from './ThemeRtlProvider';
 export * from './TranslationProvider';
