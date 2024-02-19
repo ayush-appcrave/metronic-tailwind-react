99ad65ec3a379b4b41053a77e8c9080938c2fd7f
vagapov05 <vagapov05@gmail.com>
2024-02-19 13:52:44 +0500

    feat:added the cards folder to the partials folder

--- typescript/vite/src/pages/public-profile/profiles/creator/_CreatorWorks.tsx
+++ typescript/vite/src/pages/public-profile/profiles/creator/_CreatorWorks.tsx
@@ -1,3 +1,5 @@
+import { Work } from '@/partials/cards';
+
 import { ICreatorWorksItem, ICreatorWorksItems } from './interfaces';
 
 const CreatorWorks = () => {
@@ -5,61 +7,73 @@ const CreatorWorks = () => {
     {
       url: '#',
       title: 'Urban Dreams',
-      image: 'images/content/stock/1.jpg',
+      image: '/images/content/stock/1.jpg',
       authorName: 'Cody Fisher',
-      authorAvatar: 'images/content/avatars/300-3.jpg',
+      authorAvatar: '/images/content/avatars/300-3.jpg',
       likes: 24,
       comments: 5
     },
     {
       url: '#',
       title: 'Timeless Elegance',
-      image: 'images/content/stock/2.jpg',
+      image: '/images/content/stock/2.jpg',
       authorName: 'Jenny Wilson',
-      authorAvatar: 'images/content/avatars/300-20.jpg',
+      authorAvatar: '/images/content/avatars/300-20.jpg',
       likes: 6,
       comments: 1
     },
     {
       url: '#',
       title: 'Whispered Emotions',
-      image: 'images/content/stock/7.jpg',
+      image: '/images/content/stock/7.jpg',
       authorName: 'Wade Warren',
-      authorAvatar: 'images/content/avatars/300-9.jpg',
+      authorAvatar: '/images/content/avatars/300-9.jpg',
       likes: 187,
       comments: 49
     },
     {
       url: '#',
       title: 'Golden Serenity',
-      image: 'images/content/stock/9.jpg',
+      image: '/images/content/stock/9.jpg',
       authorName: 'Albert Flores',
-      authorAvatar: 'images/content/avatars/300-15.jpg',
+      authorAvatar: '/images/content/avatars/300-15.jpg',
       likes: 60,
       comments: 13
     },
     {
       url: '#',
       title: 'Wild Beauty',
-      image: 'images/content/stock/10.jpg',
+      image: '/images/content/stock/10.jpg',
       authorName: 'Devon Lane',
-      authorAvatar: 'images/content/avatars/300-18.jpg',
+      authorAvatar: '/images/content/avatars/300-18.jpg',
       likes: 625,
       comments: 109
     },
     {
       url: '#',
       title: 'Mystic Shadows',
-      image: 'images/content/stock/1.jpg',
+      image: '/images/content/stock/1.jpg',
       authorName: 'Kathryn Murphy',
-      authorAvatar: 'images/content/avatars/300-3.jpg',
+      authorAvatar: '/images/content/avatars/300-3.jpg',
       likes: 37,
       comments: 16
     }
   ];
 
   const renderItem = (item: ICreatorWorksItem) => {
-    return <></>;
+    return (
+      <>
+        <Work
+          url={item.url}
+          title={item.title}
+          image={item.image}
+          authorName={item.authorName}
+          authorAvatar={item.authorAvatar}
+          likes={item.likes}
+          comments={item.comments}
+        />
+      </>
+    );
   };
 
   return (
---
+++ typescript/vite/src/partials/cards/interfaces.ts
@@ -0,0 +1,9 @@
+export interface IWorkProps {
+  image: string;
+  url: string;
+  title: string;
+  authorAvatar: string;
+  authorName: string;
+  likes: number;
+  comments: number;
+}
