cc1a4b252945811c135f4e29adae752567bcb9af
vagapov05 <vagapov05@gmail.com>
2024-02-22 13:12:51 +0500

    feat:added components to the feeds folder

--- typescript/vite/src/pages/public-profile/profiles/feeds/FeedsPage.tsx
+++ typescript/vite/src/pages/public-profile/profiles/feeds/FeedsPage.tsx
@@ -5,6 +5,8 @@ import { NavbarMenu } from '@/partials/menu/NavbarMenu';
 import { Navbar, NavbarActions } from '@/partials/navbar';
 import { toAbsoluteUrl } from '@/utils/Assets';
 
+import { FeedsContent } from '.';
+
 const FeedsPage = () => {
   const image = (
     <img
@@ -32,7 +34,9 @@ const FeedsPage = () => {
         </Navbar>
       </Container>
 
-      <Container>Example content</Container>
+      <Container>
+        <FeedsContent />
+      </Container>
     </>
   );
 };
