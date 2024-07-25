d1bb603c7ba0f95a7883f479974c2ccf8bc78584
vagapov05 <vagapov05@gmail.com>
2024-07-25 21:28:13 +0500

    feat: set up menu structure and path

--- typescript/vite/src/config/menu.config.tsx
+++ typescript/vite/src/config/menu.config.tsx
@@ -47,11 +47,6 @@ const MENU_SIDEBAR: MenuConfigType = [
     children: [
       {
         title: 'Profiles',
-        icon: 'profile-circle',
-        trigger: 'click',
-        dropdownProps: {
-          placement: 'bottom-start'
-        },
         children: [
           {
             title: 'Default',
@@ -140,6 +135,9 @@ const MENU_SIDEBAR: MenuConfigType = [
         collapse: true,
         collapseTitle: 'Show less',
         expandTitle: 'Show 3 more',
+        dropdownProps: {
+          placement: 'right-start'
+        },
         children: [
           {
             title: 'Campaigns - Card',
@@ -159,7 +157,7 @@ const MENU_SIDEBAR: MenuConfigType = [
   },
   {
     title: 'My Account',
-    icon: 'profile-circle',
+    icon: 'setting-2',
     children: [
       {
         title: 'Account Home',
@@ -264,139 +262,286 @@ const MENU_SIDEBAR: MenuConfigType = [
             path: '/account/members/teams'
           },
           {
-            title: 'Team Details',
-            path: '/account/members/team-details'
+            title: 'Team Info',
+            path: '/account/members/team-info'
           },
           {
-            title: 'Teams Members',
+            title: 'Members Starter',
+            path: '/account/members/members-starter'
+          },
+          {
+            title: 'Team Members',
             path: '/account/members/team-members'
           },
           {
-            title: 'Member Details',
-            path: '/account/members/member-details'
-          },
-          {
             title: 'Import Members',
-            path: '/account/members/import'
+            path: '/account/members/import-members'
           },
           {
             title: 'Roles',
             path: '/account/members/roles'
           },
           {
-            title: 'Teams Starter',
-            path: '/account/members/role-settings'
+            title: 'Permissions - Toggler',
+            path: '/account/members/permissions-toggler'
           },
           {
-            title: 'Teams Starter',
-            path: '/account/members/role-members'
+            title: 'Permissions - Check',
+            path: '/account/members/permissions-check'
+          }
+        ]
+      },
+      {
+        title: 'Integrations',
+        path: '/account/members/integrations'
+      },
+      {
+        title: 'Notifications',
+        path: '/account/members/notifications'
+      },
+      {
+        title: 'API Keys',
+        path: '/account/members/api-keys'
+      },
+      {
+        title: 'More',
+        collapse: true,
+        collapseTitle: 'Show less',
+        expandTitle: 'Show 3 more',
+        dropdownProps: {
+          placement: 'right-start'
+        },
+        children: [
+          {
+            title: 'Appearance',
+            path: '/account/members/appearance'
+          },
+          {
+            title: 'Invite a Friend',
+            path: '/account/members/invite-a-friend'
+          },
+          {
+            title: 'Activity',
+            path: '/account/members/activity'
           }
         ]
       }
     ]
   },
   {
-    title: 'Community',
-    icon: 'profile-circle',
+    title: 'Network',
+    icon: 'users',
     children: [
-      { title: 'Link', path: '#' },
-      { title: 'Link', path: '#' },
-      { title: 'Link', path: '#' }
+      { 
+        title: 'Get Started', 
+        path: '/network/get-started' 
+      },
+      {
+        title: 'User Cards',
+        children: [
+          {
+            title: 'Mini Cards',
+            path: '/network/user-cards/mini-cards'
+          },
+          {
+            title: 'Team Crew',
+            path: '/network/user-cards/team-crew'
+          },
+          {
+            title: 'Author',
+            path: '/network/user-cards/author'
+          },
+          {
+            title: 'NFT',
+            path: '/network/user-cards/nft'
+          },
+          {
+            title: 'Social',
+            path: '/network/user-cards/social'
+          },
+        ]
+      },
+      {
+        title: 'User Table',
+        children: [
+          {
+            title: 'Team Crew',
+            path: '/network/user-table/team-crew'
+          },
+          {
+            title: 'App Roster',
+            path: '/network/user-table/app-roster'
+          },
+          {
+            title: 'Market Authors',
+            path: '/network/user-table/market-authors'
+          },
+          {
+            title: 'SaaS Users',
+            path: '/network/user-table/saas-users'
+          },
+          {
+            title: 'Store Clients',
+            path: '/network/user-table/store-clients'
+          },
+          {
+            title: 'Visitors',
+            path: '/network/user-table/visitors'
+          },
+        ]
+      },
+      { 
+        title: 'Cooperations', 
+        path: '/network/cooperations',
+        soon: true,
+      },
+      { 
+        title: 'Leads', 
+        path: '/network/leads',
+        soon: true,
+      },
+      { 
+        title: 'Donators', 
+        path: '/network/donators',
+        soon: true,
+      },
     ]
   },
   {
     title: 'Authentication',
-    icon: 'profile-circle',
+    icon: 'security-user',
     children: [
-      { title: 'Link', path: '#' },
-      { title: 'Link', path: '#' },
-      { title: 'Link', path: '#' }
-    ]
-  },
-  { heading: 'Pages' },
-  {
-    title: 'Marketplace',
-    icon: 'profile-circle',
-    children: [
-      { title: 'Link', path: '#' },
-      { title: 'Link', path: '#' },
-      { title: 'Link', path: '#' }
-    ]
-  },
-  {
-    title: 'Social',
-    icon: 'profile-circle',
-    children: [
-      { title: 'Link', path: '#' },
-      { title: 'Link', path: '#' },
-      { title: 'Link', path: '#' }
-    ]
-  },
-  {
-    title: 'Company',
-    icon: 'profile-circle',
-    children: [
-      { title: 'Link', path: '#' },
-      { title: 'Link', path: '#' },
-      { title: 'Link', path: '#' }
-    ]
-  },
-  {
-    title: 'Blog',
-    icon: 'profile-circle',
-    children: [
-      { title: 'Link', path: '#' },
-      { title: 'Link', path: '#' },
-      { title: 'Link', path: '#' }
-    ]
-  },
-  { heading: 'Miscellaneous' },
-  {
-    title: 'Modals',
-    icon: 'profile-circle',
-    children: [
-      { title: 'Link', path: '#' },
-      { title: 'Link', path: '#' },
-      { title: 'Link', path: '#' }
-    ]
-  },
-  {
-    title: 'Wizards',
-    icon: 'profile-circle',
-    children: [
-      { title: 'Link', path: '#' },
-      { title: 'Link', path: '#' },
-      { title: 'Link', path: '#' }
-    ]
-  },
-  {
-    title: 'Search',
-    icon: 'profile-circle',
-    children: [
-      { title: 'Link', path: '#' },
-      { title: 'Link', path: '#' },
-      { title: 'Link', path: '#' }
+      {
+        title: 'Classic',
+        children: [
+          {
+            title: 'Sign In',
+            path: '/authentication/classic/sign-in'
+          },
+          {
+            title: 'Sign Up',
+            path: '/authentication/classic/sign-up'
+          },
+          {
+            title: '2FA',
+            path: '/authentication/classic/2fa'
+          },
+          {
+            title: 'Check Email',
+            path: '/authentication/classic/check-email'
+          },
+          {
+            title: 'Reset Password',
+            children: [
+              {
+                title: 'Enter Email',
+                path: '/authentication/classic/reset-password/enter-email'
+              },
+              {
+                title: 'Check Email',
+                path: '/authentication/classic/reset-password/check-email'
+              },
+              {
+                title: 'Change Password',
+                path: '/authentication/classic/reset-password/change-password'
+              },
+              {
+                title: 'Password is Changed',
+                path: '/authentication/classic/reset-password/password-is-changed'
+              },
+              
+            ]
+          },
+        ]
+      },
+      {
+        title: 'Branded',
+        children: [
+          {
+            title: 'Sign In',
+            path: '/authentication/branded/sign-in'
+          },
+          {
+            title: 'Sign Up',
+            path: '/authentication/branded/sign-up'
+          },
+          {
+            title: '2FA',
+            path: '/authentication/branded/2fa'
+          },
+          {
+            title: 'Check Email',
+            path: '/authentication/branded/check-email'
+          },
+          {
+            title: 'Reset Password',
+            children: [
+              {
+                title: 'Enter Email',
+                path: '/authentication/branded/reset-password/enter-email'
+              },
+              {
+                title: 'Check Email',
+                path: '/authentication/branded/reset-password/check-email'
+              },
+              {
+                title: 'Change Password',
+                path: '/authentication/branded/reset-password/change-password'
+              },
+              {
+                title: 'Password is Changed',
+                path: '/authentication/branded/reset-password/password-is-changed'
+              },
+              
+            ]
+          },
+        ]
+      },
+      { 
+        title: 'Welcome Message', 
+        path: '/authentication/welcome-message' 
+      },
+      { 
+        title: 'Account Deactivated', 
+        path: '/authentication/account-deactivated' 
+      },
+      { 
+        title: 'Error 404', 
+        path: '/authentication/error-404' 
+      },
+      { 
+        title: 'Error 500', 
+        path: '/authentication/error-500' 
+      },
     ]
   },
   { heading: 'Apps' },
   {
+    title: 'User Management',
+    icon: 'users',
+    soon: true,
+  },
+  {
     title: 'Projects',
-    icon: 'profile-circle',
-    children: [
-      { title: 'Link', path: '#' },
-      { title: 'Link', path: '#' },
-      { title: 'Link', path: '#' }
-    ]
+    icon: 'questionnaire-tablet',
+    soon: true,
   },
   {
     title: 'eCommerce',
-    icon: 'profile-circle',
-    children: [
-      { title: 'Link', path: '#' },
-      { title: 'Link', path: '#' },
-      { title: 'Link', path: '#' }
-    ]
-  }
+    icon: 'handcart',
+    soon: true,
+  },
+  { heading: 'Miscellaneous' },
+  {
+    title: 'Modals',
+    icon: 'some-files',
+    soon: true,
+  },
+  {
+    title: 'Wizards',
+    icon: 'note-2',
+    soon: true,
+  },
 ];
 
 export { MENU_SIDEBAR, PATHS_AUTH, PATHS_GENERAL };
