362400bb840c08ace3b7561642dd786c88d76843
vagapov05 <vagapov05@gmail.com>
2024-07-31 10:57:57 +0500

    feat: fixed password switch for auth

--- typescript/vite/src/auth/pages/jwt/Login.tsx
+++ typescript/vite/src/auth/pages/jwt/Login.tsx
@@ -25,18 +25,22 @@ const initialValues = {
   password: 'demo'
 };
 
-/*
-  Formik+YUP+Typescript:
-  https://jaredpalmer.com/formik/docs/tutorial#getfieldprops
-  https://medium.com/@maurice.de.beijer/yup-validation-and-typescript-and-formik-6c342578a20e
-*/
-
 const Login = () => {
   const [loading, setLoading] = useState(false);
   const { login } = useAuthContext();
   const navigate = useNavigate();
   const location = useLocation();
   const from = location.state?.from?.pathname || '/';
+  const [showPassword, setShowPassword] = useState(false);
+  const [rememberMe, setRememberMe] = useState(false);
+
+  const togglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleRememberMeChange = (event: { target: { checked: boolean | ((prevState: boolean) => boolean); }; }) => {
+    setRememberMe(event.target.checked);
+  };
 
   const formik = useFormik({
     initialValues,
@@ -65,51 +69,24 @@ const Login = () => {
         className="card-body flex flex-col gap-5 p-10"
         onSubmit={formik.handleSubmit}
         noValidate
-        id="kt_login_signin_form"
       >
-        {/* begin::Heading */}
         <div className="text-center mb-2.5">
           <h3 className="text-lg font-semibold text-gray-900 leading-none mb-2.5">Sign in</h3>
           <div className="flex items-center justify-center font-medium">
             <span className="text-2sm text-gray-600 me-1.5">Need an account?</span>
-            <Link to="/auth/registration" className="text-2sm link">
-              Sign up
-            </Link>
+            <Link to="/auth/registration" className="text-2sm link">Sign up</Link>
           </div>
         </div>
-        {/* begin::Heading */}
-
-        {/* {formik.status ? (
-          <div className="mb-lg-15 alert alert-danger">
-            <div className="alert-text font-weight-bold">{formik.status}</div>
-          </div>
-        ) : (
-          <div className="mb-10 bg-light-info p-8 rounded">
-            <div className="text-info">
-              Use account <strong>admin@demo.com</strong> and password <strong>demo</strong> to
-              continue.
-            </div>
-          </div>
-        )} */}
         
         <div className="grid grid-cols-2 gap-2.5">
           <a href="#" className="btn btn-light btn-sm justify-center">
-            <img
-              src={toAbsoluteUrl('/media/brand-logos/google.svg')}
-              className="size-3.5 shrink-0"
-            />
+            <img src={toAbsoluteUrl('/media/brand-logos/google.svg')} className="size-3.5 shrink-0" />
             Use Google
           </a>
 
           <a href="#" className="btn btn-light btn-sm justify-center">
-            <img
-              src={toAbsoluteUrl('/media/brand-logos/apple-black.svg')}
-              className="size-3.5 shrink-0 dark:hidden"
-            />
-            <img
-              src={toAbsoluteUrl('/media/brand-logos/apple-white.svg')}
-              className="size-3.5 shrink-0 light:hidden"
-            />
+            <img src={toAbsoluteUrl('/media/brand-logos/apple-black.svg')} className="size-3.5 shrink-0 dark:hidden" />
+            <img src={toAbsoluteUrl('/media/brand-logos/apple-white.svg')} className="size-3.5 shrink-0 light:hidden" />
             Use Apple
           </a>
         </div>  
@@ -120,7 +97,6 @@ const Login = () => {
           <span className="border-t border-gray-200 w-full"></span> 
         </div>
 
-        {/* begin::Form group */}
         <div className="flex flex-col gap-1">
           <label className="form-label text-gray-900">Email</label>
           <label className="input" data-toggle-password="true">
@@ -139,25 +115,17 @@ const Login = () => {
               autoComplete="off"
             />
           </label>
-          {formik.touched.email && formik.errors.email && (
-            <div className="fv-plugins-message-container">
-              <span role="alert">{formik.errors.email}</span>
-            </div>
-          )}
+          {formik.touched.email && formik.errors.email && (<span role="alert">{formik.errors.email}</span>)}
         </div>
-        {/* end::Form group */}
 
-        {/* begin::Form group */}
         <div className="flex flex-col gap-1">
           <div className="flex items-center justify-between gap-1">
             <label className="form-label text-gray-900">Password</label>
-            <Link to="/auth/forgot-password" className="text-2sm link shrink-0">
-              Forgot Password?
-            </Link>
+            <Link to="/auth/forgot-password" className="text-2sm link shrink-0">Forgot Password?</Link>
           </div>
           <label className="input" data-toggle-password="true">
             <input
-              type="password"
+              type={showPassword ? 'text' : 'password'}
               placeholder="Enter Password"
               autoComplete="off"
               {...formik.getFieldProps('password')}
@@ -171,30 +139,26 @@ const Login = () => {
                 }
               )}
             />
-            {formik.touched.password && formik.errors.password && (
-              <div className="fv-plugins-message-container">
-                <div className="fv-help-block">
-                  <span role="alert">{formik.errors.password}</span>
-                </div>
-              </div>
-            )}
-            <button className="btn btn-icon" data-toggle-password-trigger="true">
-              <KeenIcon icon="eye" className="text-gray-500 toggle-password-active:hidden" />
-              <KeenIcon icon="eye-slash" className="text-gray-500 hidden toggle-password-active:block" />
+            {formik.touched.password && formik.errors.password && (<span role="alert">{formik.errors.password}</span>)}
+            <button className="btn btn-icon" onClick={togglePassword}>
+            <KeenIcon icon="eye" className={clsx('text-gray-500', { 'hidden': showPassword })} />
+            <KeenIcon icon="eye-slash" className={clsx('text-gray-500', { 'hidden': !showPassword })} />
             </button>
           </label>
         </div>
-        {/* end::Form group */}
 
         <label className="checkbox-group">
-          <input className="checkbox checkbox-sm" name="check" type="checkbox" value="1"/>
+          <input
+            className="checkbox checkbox-sm"
+            type="checkbox"
+            checked={rememberMe}
+            onChange={handleRememberMeChange}
+          />
           <span className="checkbox-label">Remember me</span>
         </label>
 
-        {/* begin::Action */}
         <button
           type="submit"
-          id="kt_sign_in_submit"
           className="btn btn-primary flex justify-center grow"
           disabled={formik.isSubmitting || !formik.isValid}
         >
@@ -206,7 +170,6 @@ const Login = () => {
             </span>
           )}
         </button>
-        {/* end::Action */}
       </form>
     </div>
   );
