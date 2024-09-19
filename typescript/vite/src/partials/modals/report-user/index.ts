dc548b4547c4158b56e4292159ff4c4f2858c8ed
sean0205 <dr.creator@gmail.com>
2024-09-19 14:44:00 +0500

    latest code

--- typescript/vite/src/partials/modals/report-user/index.ts
+++ typescript/vite/src/partials/modals/report-user/index.ts
@@ -1,5 +1,2 @@
 export * from './ModalReportUser';
-export * from './ModalReportUserOptions';
-export * from './ModalReportUserNotice';
-export * from './ModalReportUserFooter';
 export * from './types';
