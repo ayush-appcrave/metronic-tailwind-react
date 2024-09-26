92ec0b3f57edb36dfa3ea213b5d97bb4048bf17e
Azim Saipov <azim@Azims-MacBook-Air.local>
2024-09-26 19:08:27 +0500

    latest code

--- typescript/vite/src/pages/network/user-table/app-roster/NetworkAppRosterContent.tsx
+++ typescript/vite/src/pages/network/user-table/app-roster/NetworkAppRosterContent.tsx
@@ -1,9 +1,10 @@
+import { Users } from './blocks/users';
 import { MiscFaq, MiscHelp2 } from '@/partials/misc';
 
 const NetworkAppRosterContent = () => {
   return (
     <div className="grid gap-5 lg:gap-7.5">
-      {/* {{ theme.page('_users') }} */}
+      <Users />
 
       <MiscFaq />
 
