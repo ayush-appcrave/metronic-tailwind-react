6310df07c23930e2aefa8242ba6c9d717cf8544e
laurisstepanovs <33573735+laurisstepanovs@users.noreply.github.com>
2024-09-30 18:46:47 +0300

    Change vite app base path

--- typescript/vite/vite.config.ts
+++ typescript/vite/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/metronic',
+  base: '/metronic/tailwind/react',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
