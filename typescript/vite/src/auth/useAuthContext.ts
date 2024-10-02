36d0192ad4de1020c0cbc0cf9b557b215d552600
laurisstepanovs <33573735+laurisstepanovs@users.noreply.github.com>
2024-10-03 01:00:57 +0300

    Remove comented auth provider imports

--- typescript/vite/src/auth/useAuthContext.ts
+++ typescript/vite/src/auth/useAuthContext.ts
@@ -1,8 +1,5 @@
 import { useContext } from 'react';
-
 import { AuthContext } from './providers/JWTProvider';
-// import { AuthContext } from './providers/Auth0Provider';
-// import { AuthContext } from './providers/FirebaseProvider';
 
 export const useAuthContext = () => {
   const context = useContext(AuthContext);
