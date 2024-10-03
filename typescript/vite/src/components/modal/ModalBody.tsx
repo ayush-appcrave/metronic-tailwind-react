6832df7ca479e427060ea020244060ee37d1e59c
sean0205 <dr.creator@gmail.com>
2024-10-03 09:49:14 +0500

    latest code

--- typescript/vite/src/components/modal/ModalBody.tsx
+++ typescript/vite/src/components/modal/ModalBody.tsx
@@ -1,16 +1,17 @@
-import { forwardRef, ReactNode } from 'react';
+import { forwardRef, ReactNode, CSSProperties } from 'react';
 
 interface ModalModalProps {
   className?: string;
   children: ReactNode;
   tabIndex?: number;
+  style?: CSSProperties; // Accept inline styles as a prop
 }
 
 // Forwarding ref to ensure this component can hold a ref
 const ModalBody = forwardRef<HTMLDivElement, ModalModalProps>(
-  ({ className, children, tabIndex = -1 }, ref) => {
+  ({ className, children, style, tabIndex = -1 }, ref) => {
     return (
-      <div ref={ref} tabIndex={tabIndex} className={`modal-body ${className}`}>
+      <div ref={ref} tabIndex={tabIndex} className={`modal-body ${className}`} style={style}>
         {children}
       </div>
     );
