0a11d8bdb6c7854fb5e1a8b3e7d54e4095c905fd
Azim Saipov <azim@Azims-MacBook-Air.local>
2024-10-04 18:31:41 +0500

    latest code

--- typescript/vite/src/pages/network/user-table/store-clients/NetworkStoreClientsPage.tsx
+++ typescript/vite/src/pages/network/user-table/store-clients/NetworkStoreClientsPage.tsx
@@ -10,33 +10,38 @@ import {
 } from '@/partials/toolbar';
 
 import { NetworkStoreClientsContent } from '.';
+import { useLayout } from '@/providers'; 
 
 const NetworkStoreClientsPage = () => {
+  const { currentLayout } = useLayout ();
+
   return (
     <Fragment>
-      <Container>
-        <Toolbar>
-          <ToolbarHeading>
-            <ToolbarPageTitle />
-            <ToolbarDescription>
-              <div className="flex items-center flex-wrap gap-1.5 font-medium">
-                <span className="text-md text-gray-600">All Members:</span>
-                <span className="text-md text-ray-800 font-semibold me-2">49,053</span>
-                <span className="text-md text-gray-600">Pro Licenses</span>
-                <span className="text-md text-gray-800 font-semibold">724</span>
-              </div>
-            </ToolbarDescription>
-          </ToolbarHeading>
-          <ToolbarActions>
-            <a href="#" className="btn btn-sm btn-light">
-              Import CSV
-            </a>
-            <a href="#" className="btn btn-sm btn-primary">
-              Add Member
-            </a>
-          </ToolbarActions>
-        </Toolbar>
-      </Container>
+      {currentLayout?.name === 'demo1-layout' && ( 
+        <Container>
+          <Toolbar>
+            <ToolbarHeading>
+              <ToolbarPageTitle />
+              <ToolbarDescription>
+                <div className="flex items-center flex-wrap gap-1.5 font-medium">
+                  <span className="text-md text-gray-600">All Members:</span>
+                  <span className="text-md text-ray-800 font-semibold me-2">49,053</span>
+                  <span className="text-md text-gray-600">Pro Licenses</span>
+                  <span className="text-md text-gray-800 font-semibold">724</span>
+                </div>
+              </ToolbarDescription>
+            </ToolbarHeading>
+            <ToolbarActions>
+              <a href="#" className="btn btn-sm btn-light">
+                Import CSV
+              </a>
+              <a href="#" className="btn btn-sm btn-primary">
+                Add Member
+              </a>
+            </ToolbarActions>
+          </Toolbar>
+        </Container>
+      )}
 
       <Container>
         <NetworkStoreClientsContent />
