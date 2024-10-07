a0e49cea9354d883361b3e01b7ddb2981e8797a6
sean0205 <dr.creator@gmail.com>
2024-10-07 23:26:29 +0500

    latest code

--- typescript/vite/src/layouts/demo3/Demo3Layout.tsx
+++ typescript/vite/src/layouts/demo3/Demo3Layout.tsx
@@ -1,11 +1,21 @@
 import useBodyClasses from '@/hooks/useBodyClasses';
 import { Demo3LayoutProvider, Main } from '.';
+import { useEffect } from 'react';
+import { useSettings } from '@/providers';
 
 const Demo3Layout = () => {
+  const { updateSettings } = useSettings();
+
   // Using the custom hook to set multiple CSS variables and class properties
   useBodyClasses(
     `[--tw-page-bg:var(--tw-light)] [--tw-page-bg-dark:var(--tw-coal-500)] [--tw-header-height-default:100px] [[data-sticky-header=on]&]:[--tw-header-height:60px] [--tw-header-height:--tw-header-height-default] bg-[--tw-page-bg] dark:bg-[--tw-page-bg-dark]`
   );
+
+  useEffect(() => {
+    updateSettings({
+      container: 'fluid'
+    });
+  }, [updateSettings]);
 
   return (
     // Providing layout context and rendering the main content
