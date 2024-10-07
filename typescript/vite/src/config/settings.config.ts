a889ac9affd55c1ef997c51f4180d88510e335b9
sean0205 <dr.creator@gmail.com>
2024-10-07 22:26:22 +0500

    latest code

--- typescript/vite/src/config/settings.config.ts
+++ typescript/vite/src/config/settings.config.ts
@@ -16,7 +16,7 @@ export interface ISettings {
 const defaultSettings: ISettings = {
   mode: 'light', // Default to light mode for the application
   keeniconsStyle: 'filled', // Default to using filled KeenIcons style
-  container: 'fixed', // Default container layout is set to fixed
+  container: 'fixed' // Default container layout is set to fixed
 };
 
 export { defaultSettings };
