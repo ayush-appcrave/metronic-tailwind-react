3962ec8c418e1c9a44a3e281fe44a46455c5f2af
sean0205 <dr.creator@gmail.com>
2024-10-10 12:34:58 +0500

    demo5 main classes fix

--- typescript/vite/src/layouts/demo5/Demo5Layout.tsx
+++ typescript/vite/src/layouts/demo5/Demo5Layout.tsx
@@ -4,7 +4,7 @@ import { Demo5LayoutProvider, Main } from '.';
 const Demo5Layout = () => {
   // Using the custom hook to set multiple CSS variables and class properties
   useBodyClasses(
-    `[--tw-page-bg:var(--tw-light)] [--tw-page-bg-dark:var(--tw-coal-500)] [--tw-header-height-default:100px] [[data-sticky-header=on]&]:[--tw-header-height:60px] [--tw-header-height:--tw-header-height-default] bg-[--tw-page-bg] dark:bg-[--tw-page-bg-dark]`
+    '[--tw-header-height:54px] [--tw-sidebar-width:200px] [--tw-header-bg:var(--tw-light-active)] [--tw-header-bg-dark:var(--tw-gray-200)] bg-light dark:bg-coal-500'
   );
 
   return (
--- typescript/vite/src/routing/AppRouting.tsx
+++ typescript/vite/src/routing/AppRouting.tsx
@@ -75,7 +75,7 @@ import {
 
 import { AuthPage, useAuthContext } from '../auth';
 import { RequireAuth } from '../auth/RequireAuth';
-import { Demo3Layout } from '../layouts/demo3';
+import { Demo5Layout } from '../layouts/demo5';
 import { ErrorsRouting } from '../errors';
 import { useLoaders } from '../providers/LoadersProvider';
 import { AuthenticationWelcomeMessagePage } from '@/pages/authentication/welcome-message/AuthenticationWelcomeMessagePage';
@@ -126,7 +126,7 @@ const AppRouting = (): ReactElement => {
   return (
     <Routes>
       <Route element={<RequireAuth />}>
-        <Route element={<Demo3Layout />}>
+        <Route element={<Demo5Layout />}>
           <Route path="/" element={<DefaultPage />} />
           <Route path="/dark-sidebar" element={<DarkSidebarPage />} />
           <Route path="/public-profile/profiles/default" element={<ProfileDefaultPage />} />
