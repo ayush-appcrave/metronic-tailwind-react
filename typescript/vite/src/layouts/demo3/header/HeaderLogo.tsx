fc977a2189594806917815aff8111f63bf93ecc5
Azim Saipov <azim@Azims-MacBook-Air.local>
2024-10-10 12:00:41 +0500

    latest code

--- typescript/vite/src/layouts/demo3/header/HeaderLogo.tsx
+++ typescript/vite/src/layouts/demo3/header/HeaderLogo.tsx
@@ -33,72 +33,74 @@ const HeaderLogo = () => {
   }, [pathname]);
 
   return (
-    <div className="flex items-center gap-2 lg:gap-4">
-      <button
-        type="button"
-        onClick={handleSidebarOpen}
-        className="btn btn-icon btn-icon-lg hover:text-primary btn-sm -ms-2 lg:hidden"
-      >
-        <KeenIcon icon="menu" />
-      </button>
+    <div className="flex items-center mr-1">
+      <div className="flex items-center justify-center lg:w-[--tw-sidebar-width] gap-2 shrink-0">
+        <button
+          type="button"
+          onClick={handleSidebarOpen}
+          className="btn btn-icon btn-light btn-clear btn-sm -ms-2 lg:hidden"
+        >
+          <KeenIcon icon="menu" />
+        </button>
 
-      <Link to="/" className="shrink-0 -ml-0.5">
-        <img
-          src={toAbsoluteUrl('/media/app/mini-logo-primary.svg')}
-          className="dark:hidden min-h-[24px]"
-          alt="logo"
-        />
-        <img
-          src={toAbsoluteUrl('/media/app/mini-logo-primary-dark.svg')}
-          className="hidden dark:inline-block min-h-[24px]"
-          alt="logo"
-        />
-      </Link>
+        <Link to="/" className="mx-1">
+          <img
+            src={toAbsoluteUrl('/media/app/mini-logo-primary.svg')}
+            className="dark:hidden min-h-[24px]"
+            alt="logo"
+          />
+          <img
+            src={toAbsoluteUrl('/media/app/mini-logo-primary-dark.svg')}
+            className="hidden dark:inline-block min-h-[24px]"
+            alt="logo"
+          />
+        </Link>
 
-      <div className="flex items-center">
-        <h3 className="text-gray-700 text-base hidden md:block">MetronicTeam</h3>
-        <span className="text-sm text-gray-400 font-medium px-2.5 hidden md:inline">/</span>
+        <div className="flex items-center">
+          <h3 className="text-gray-700 text-base hidden md:block">MetronicTeam</h3>
+          <span className="text-sm text-gray-400 font-medium px-2.5 hidden md:inline">/</span>
 
-        <Menu className="menu-default">
-          <MenuItem
-            toggle="dropdown"
-            trigger="hover"
-            dropdownProps={{
-              placement: 'bottom-start',
-              modifiers: [
-                {
-                  name: 'offset',
-                  options: {
-                    offset: [0, 10] // [skid, distance]
+          <Menu className="menu-default">
+            <MenuItem
+              toggle="dropdown"
+              trigger="hover"
+              dropdownProps={{
+                placement: 'bottom-start',
+                modifiers: [
+                  {
+                    name: 'offset',
+                    options: {
+                      offset: [0, 10] // [skid, distance]
+                    }
                   }
-                }
-              ]
-            }}
-          >
-            <MenuToggle>
-              <MenuTitle className="!text-gray-900 !text-base !font-medium">
-                {selectedMenuItem.title}
-              </MenuTitle>
-              <MenuArrow>
-                <KeenIcon icon="down" />
-              </MenuArrow>
-            </MenuToggle>
-            <MenuSub className="menu-default">
-              {MENU_ROOT.map((item, index) => (
-                <MenuItem key={index} className={item === selectedMenuItem ? 'active' : ''}>
-                  <MenuLink path={item.path}>
-                    {item.icon && (
-                      <MenuIcon>
-                        <KeenIcon icon={item.icon} />
-                      </MenuIcon>
-                    )}
-                    <MenuTitle>{item.title}</MenuTitle>
-                  </MenuLink>
-                </MenuItem>
-              ))}
-            </MenuSub>
-          </MenuItem>
-        </Menu>
+                ]
+              }}
+            >
+              <MenuToggle>
+                <MenuTitle className="!text-gray-900 !text-base !font-medium">
+                  {selectedMenuItem.title}
+                </MenuTitle>
+                <MenuArrow>
+                  <KeenIcon icon="down" />
+                </MenuArrow>
+              </MenuToggle>
+              <MenuSub className="menu-default">
+                {MENU_ROOT.map((item, index) => (
+                  <MenuItem key={index} className={item === selectedMenuItem ? 'active' : ''}>
+                    <MenuLink path={item.path}>
+                      {item.icon && (
+                        <MenuIcon>
+                          <KeenIcon icon={item.icon} />
+                        </MenuIcon>
+                      )}
+                      <MenuTitle>{item.title}</MenuTitle>
+                    </MenuLink>
+                  </MenuItem>
+                ))}
+              </MenuSub>
+            </MenuItem>
+          </Menu>
+        </div>
       </div>
     </div>
   );
