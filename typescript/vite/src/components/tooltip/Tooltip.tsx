47c4e5e067218470bd5ab921c7b8b3c6f2fba864
sean0205 <dr.creator@gmail.com>
2024-10-12 14:24:15 +0500

    latest code

--- typescript/vite/src/components/tooltip/Tooltip.tsx
+++ typescript/vite/src/components/tooltip/Tooltip.tsx
@@ -1,27 +1,15 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Tooltip, { tooltipClasses, TooltipProps } from '@mui/material/Tooltip';
-
-// Styled Tooltip to customize the popper and tooltip content
-const StyledTooltip = styled(Tooltip)(() => ({
-  [`& .${tooltipClasses.tooltip}`]: {
-    backgroundColor: 'var(--tw-tooltip-background-color)', // Custom background
-    color: 'var(--tw-tooltip-text-color)', // Custom text color
-    boxShadow: 'var(--tw-tooltip-box-shadow)', // Custom shadow
-    padding: '0.375rem 0.6rem', // Custom padding
-    fontSize: '12px', // Custom font size
-    borderRadius: '0.25rem' // Rounded corners
-  },
-  [`& .${tooltipClasses.arrow}`]: {
-    color: 'var(--tw-tooltip-background-color)' // Match arrow color with tooltip background
-  }
-}));
+import Tooltip, { TooltipProps } from '@mui/material/Tooltip';
 
 // Tooltip component that applies utility classes and custom styles
 const DefaultTooltip = ({ ...props }: TooltipProps) => (
-  <StyledTooltip
+  <Tooltip
+    TransitionProps={{ timeout: 300 }}
     {...props}
-    classes={{ tooltip: '!text-white !rounded-md !text-xs !font-normal' }}
+    classes={{
+      tooltip:
+        '!text-white !rounded-md !text-xs !font-normal !bg-[--tw-tooltip-background-color] !box-shadow[--tw-tooltip-box-shadow] !border[--tw-tooltip-border] !p-[0.375rem 0.6rem]'
+    }}
   />
 );
 
