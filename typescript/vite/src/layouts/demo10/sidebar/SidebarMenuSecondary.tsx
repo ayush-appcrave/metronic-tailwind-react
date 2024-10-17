e5ad5f7f397a70dc179cced1a008d6b452bdfb5d
sean0205 <dr.creator@gmail.com>
2024-10-17 13:47:34 +0500

    latest code

--- typescript/vite/src/layouts/demo10/sidebar/SidebarMenuSecondary.tsx
+++ typescript/vite/src/layouts/demo10/sidebar/SidebarMenuSecondary.tsx
@@ -1,30 +1,24 @@
 import React from 'react';
 import clsx from 'clsx';
 import { toAbsoluteUrl } from '@/utils';
-import { 
-  Menu,
-  MenuItem,
-  MenuLink,
-  MenuTitle,
-  MenuIcon
-} from '@/components/menu'; 
+import { Menu, MenuItem, MenuLink, MenuTitle, MenuIcon } from '@/components/menu';
 
 const items = [
   {
     logo: 'x-dark.svg',
     title: '@keenthemes',
-    path: 'https://keenthemes.com/metronic/tailwind/docs/',
+    path: 'https://keenthemes.com/metronic/tailwind/docs/'
   },
   {
     logo: 'slack.svg',
     title: '@keenthemes_hub',
-    path: 'https://github.com/keenthemes/',
+    path: 'https://github.com/keenthemes/'
   },
   {
     logo: 'figma.svg',
     title: 'metronic',
-    path: 'https://keenthemes.com/metronic/tailwind/docs/',
-  },
+    path: 'https://keenthemes.com/metronic/tailwind/docs/'
+  }
 ];
 
 const SidebarMenuSecondary = () => {
@@ -33,17 +27,14 @@ const SidebarMenuSecondary = () => {
       <h3 className="text-sm text-gray-500 uppercase ps-5 inline-block mb-3">Outline</h3>
       <Menu className="flex flex-col w-full gap-1.5 px-3.5">
         {items.map((item, index) => (
-          <MenuItem key={index} className={clsx("menu-item", { active: item.active })}>
-            <MenuLink 
-              path={item.path}   
-              className="py-1 px-2.5"   
-            >
+          <MenuItem key={index} className="menu-item">
+            <MenuLink path={item.path} className="py-1 px-2.5">
               <MenuIcon>
                 <span className="rounded-md flex items-center justify-center size-7 bg-coal-black menu-link-hover:border-none me-2">
-                  <img  
-                    src={toAbsoluteUrl(`/media/brand-logos/${item.logo}`)} 
-                    className="size-3.5" 
-                    alt={item.title} 
+                  <img
+                    src={toAbsoluteUrl(`/media/brand-logos/${item.logo}`)}
+                    className="size-3.5"
+                    alt={item.title}
                   />
                 </span>
               </MenuIcon>
