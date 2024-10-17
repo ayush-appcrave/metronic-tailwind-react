fa7e603670ba7c52c3d79f7d0472956aacd50a57
sean0205 <dr.creator@gmail.com>
2024-10-17 16:29:21 +0500

    latest code

--- typescript/vite/src/routing/AppRoutingSetup.tsx
+++ typescript/vite/src/routing/AppRoutingSetup.tsx
@@ -75,7 +75,7 @@ import {
 
 import { AuthPage } from '@/auth';
 import { RequireAuth } from '@/auth/RequireAuth';
-import { Demo5Layout } from '@/layouts/demo5';
+import { Demo1Layout } from '@/layouts/demo1';
 import { ErrorsRouting } from '@/errors';
 import {
   AuthenticationWelcomeMessagePage,
@@ -87,7 +87,7 @@ const AppRoutingSetup = (): ReactElement => {
   return (
     <Routes>
       <Route element={<RequireAuth />}>
-        <Route element={<Demo5Layout />}>
+        <Route element={<Demo1Layout />}>
           <Route path="/" element={<DefaultPage />} />
           <Route path="/dark-sidebar" element={<Demo1DarkSidebarPage />} />
           <Route path="/public-profile/profiles/default" element={<ProfileDefaultPage />} />
