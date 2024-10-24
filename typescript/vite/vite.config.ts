51c7f7938595055ce69d9a09fd41968a17d4606c
sean0205 <dr.creator@gmail.com>
2024-10-24 11:59:03 +0500

    rtl fix

--- typescript/vite/vite.config.ts
+++ typescript/vite/vite.config.ts
@@ -1,9 +1,15 @@
 import { fileURLToPath, URL } from 'node:url';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()]
+    }
+  },
   base: '/metronic/tailwind/react',
   resolve: {
     alias: {
